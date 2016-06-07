be5a1d34c2ae2391fc8856955622a25630cf0f70
gengj <35285770@qq.com>
2016-06-07 12:59:35 +0800

    TTTTEST

---
+++ TEST_GITHUB/TEST_GITHUB/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TEST_GITHUB
+//
+//  Created by GengMAX on 16/6/7.
+//  Copyright © 2016年 GengMAX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, GITHUB!");
+    }
+    return 0;
+}
